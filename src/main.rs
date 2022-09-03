f05bfa5b1e4c202869db6dfdfa4e7ddf49cd35e8
Generatores <bruno.ruiz@bbrr.solutions>
2022-09-02 19:05:21 -0700

    displaying values as arguments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 fn main() {
     // To print text on the command line we use the println! function
     println!("Hello, world!");
+
+    // If we add a {} inside a "" the values to be displayed will be passed as arguments
+    println!("{} and {}", 1, 2);
 }
