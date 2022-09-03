4674e711f5f2f8abecb7996d4f9d97a41730ad5e
Generatores <bruno.ruiz@bbrr.solutions>
2022-09-02 19:08:26 -0700

    displaying a variable

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,16 @@ fn main() {
 
     // If we add a {} inside a "" the values to be displayed will be passed as arguments
     println!("{} and {}", 1, 2);
+
+    let value: &str = "first value";
+
+    // a value cant be displayed alone in a println!
+    // this displays an error
+    //println!(value);
+
+    // this is the proper way
+    println!(
+        "To display a variable it should be inside a curly bracket '{}'",
+        value
+    )
 }
